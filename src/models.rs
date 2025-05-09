82c3fa55b0b0111e99752bbdfce24c5fd74f04c4
博ノ助（Hacnoshuke） <hacnosuke@gmail.com>
2025-05-09 11:50:53 +0900

    Progress bar enhancements: include elapsed time in ETA and display loss during training and validation

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,7 @@
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use ndarray::ArrayD;
 use ndarray_rand::rand;
+use rand::seq::SliceRandom;
 
 use crate::{losses::LossFunction, networks::layer::Layer, optimizers::Optimizer};
 
@@ -37,7 +38,7 @@ impl BaseModel {
     let overall_pb = m.add(ProgressBar::new(total_steps));
     overall_pb.set_style(
       ProgressStyle::default_bar()
-        .template("Total Progress: [{bar:40.cyan/blue}] {pos}/{len} ({eta})")
+        .template("Total Progress: [{bar:40.cyan/blue}] {pos}/{len} ({{elapsed_precise}} ETA:{{eta}}) {{msg}}")
         .unwrap()
         .progress_chars("##-"),
     );
@@ -47,7 +48,7 @@ impl BaseModel {
       epoch_pb.set_style(
         ProgressStyle::default_bar()
           .template(&format!(
-            "[Train] Epoch #{}: [{{bar:40.green/black}}] {{pos}}/{{len}} ({{eta}})",
+            "[Train] Epoch #{}: [{{bar:40.green/black}}] {{pos}}/{{len}} ({{elapsed_precise}} ETA:{{eta}}) {{msg}}",
             epoch
           ))
           .unwrap()
@@ -57,6 +58,7 @@ impl BaseModel {
         let y_pred = self.main_layer.forward(x_train.clone());
         let loss_value = self.loss_func.forward(y_pred.clone(), y_train.clone());
         // println!("Epoch {}: Loss: {}", epoch, loss_value);
+        epoch_pb.set_message(format!("Loss: {}", loss_value));
         train_losses.push(loss_value);
         let grad = self.loss_func.backward(y_pred.clone(), y_train.clone());
         self.main_layer.backward(grad);
@@ -70,7 +72,7 @@ impl BaseModel {
       epoch_pb.set_style(
         ProgressStyle::default_bar()
           .template(&format!(
-            "[Train] Epoch #{}: [{{bar:40.green/black}}] {{pos}}/{{len}} ({{eta}})",
+            "[Validation] Epoch #{}: [{{bar:40.green/black}}] {{pos}}/{{len}} ({{elapsed_precise}} ETA:{{eta}}) {{msg}}",
             epoch
           ))
           .unwrap()
@@ -80,6 +82,7 @@ impl BaseModel {
         let y_pred = self.main_layer.forward(x_val.clone());
         let loss_value = self.loss_func.forward(y_pred.clone(), y_val.clone());
         // println!("Validation Loss: {}", loss_value);
+        epoch_pb.set_message(format!("Loss: {}", loss_value));
         val_losses.push(loss_value);
         epoch_pb.inc(1);
         overall_pb.inc(1);
@@ -107,7 +110,7 @@ impl BaseModel {
     let overall_pb = m.add(ProgressBar::new(total_steps));
     overall_pb.set_style(
       ProgressStyle::default_bar()
-        .template("Total Progress: [{bar:40.cyan/blue}] {pos}/{len} ({eta})")
+        .template(&format!("Total Progress: [{{bar:40.cyan/blue}}] {{pos}}/{{len}} ({{elapsed_precise}} ETA:{{eta}}) {{msg}}"))
         .unwrap()
         .progress_chars("##-"),
     );
@@ -117,7 +120,7 @@ impl BaseModel {
       epoch_pb.set_style(
         ProgressStyle::default_bar()
           .template(&format!(
-            "Epoch #{}: [{{bar:40.green/black}}] {{pos}}/{{len}} ({{eta}})",
+            "[Train] Epoch #{}: [{{bar:40.green/black}}] {{pos}}/{{len}} ({{elapsed_precise}} ETA:{{eta}}) {{msg}}",
             epoch
           ))
           .unwrap()
@@ -131,13 +134,14 @@ impl BaseModel {
         self.main_layer.backward(grad);
         self.optimizer.update(self.main_layer.params_mut());
 
-        let mut val_losses = vec![];
-        for (x_val, y_val) in val_data {
-          let y_pred = self.main_layer.forward(x_val.clone());
-          let loss_value = self.loss_func.forward(y_pred.clone(), y_val.clone());
-          val_losses.push(loss_value);
-        }
-        let val_loss = val_losses.iter().sum::<f64>() / val_losses.len() as f64;
+        let (x_val, y_val) = val_data.choose(&mut rand::thread_rng()).unwrap();
+        let y_pred = self.main_layer.forward(x_val.clone());
+        let val_loss = self.loss_func.forward(y_pred.clone(), y_val.clone());
+        epoch_pb.set_message(format!(
+          "Train Loss: {} Validation Loss: {}",
+          loss_value, val_loss
+        ));
+
         result.push((loss_value, val_loss));
 
         epoch_pb.inc(1);
